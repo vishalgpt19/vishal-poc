c025ae0defc43d53eaf25dbb000cd16ad21f86dd
Vishal Gupta <vishal.gupta@hashedin.com>
2021-03-18 15:34:48 +0530

    Updated Message

--- python-app/main.py
+++ python-app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python App"
+    return "Hello from Python Appsss"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
